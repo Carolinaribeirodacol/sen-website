3e46fff75c0c3eb3b59f17e6c6f2aae77bc50d4d
Carolinaribeirodacol <carolinaribeirodacol@gmail.com>
2023-08-15 23:12:14 -0300

    pagina de post com busca

--- frontend/src/components/commom/SearchBar.tsx
+++ frontend/src/components/commom/SearchBar.tsx
@@ -15,18 +15,18 @@ export const SearchBar = () => {
   return (
     <div>
         <form onSubmit={handleSubmit}>   
-            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
+            <label className="mb-2 text-sm font-medium text-gray-100 sr-only dark:text-white">Buscar</label>
             <div className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <svg className="w-4 h-4 text-gray-100 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
                 <input
                     type="search" id="default-search" 
-                    className="block w-full p-4 pl-10 text-sm text-gray-900 border 
-                        border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-900 focus:border-purple-900 dark:bg-gray-700 dark:border-gray-600 
-                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-900 dark:focus:border-purple-900" 
+                    className="block w-full p-4 px-14 text-sm border 
+                        rounded-lg bg-gray-400 focus:ring-purple-900 dark:bg-gray-400 
+                        dark:placeholder-gray-300 dark:text-white dark:focus:border-purple-900" 
                     placeholder="natureza, amizade..."
                     required
                     name="fieldTagName"
@@ -34,7 +34,7 @@ export const SearchBar = () => {
                     onChange={(e) => setTag(e.target.value)}
                 />
                 <button type="submit" className="absolute right-2.5 bottom-2.5 bg-purple-900 hover:bg-purple-900 
-                    focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+                    focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg 
                     text-sm px-4 py-2 dark:bg-white text-purple-900 hover:text-white dark:hover:bg-purple-900 dark:focus:ring-purple-900"
                 >Buscar</button>
             </div>
