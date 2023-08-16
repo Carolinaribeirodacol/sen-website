dfce4c6cd6cd6254614a27708098634fbbf1d02a
Lucas de Andrade <bandradelucas@gmail.com>
2023-08-15 23:46:35 -0300

    ckeditor adicionado

---
+++ backend/src/admin/app.js
@@ -0,0 +1,17 @@
+import ckeditor5Dll from "ckeditor5/build/ckeditor5-dll.js";
+import ckeditor5MrkdownDll from "@ckeditor/ckeditor5-markdown-gfm/build/markdown-gfm.js";
+
+const config = {
+  locales: [
+    'pt-BR',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
