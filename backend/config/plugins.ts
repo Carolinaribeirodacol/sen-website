e15d609e93f07638a33794fb6bc0eb831986b8b9
Carolinaribeirodacol <carolinaribeirodacol@gmail.com>
2024-01-08 01:10:52 -0300

    sendgrid api

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -3,4 +3,16 @@ export default ({ env }) => ({
   'google-auth': {
     enabled: true,
   },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'noreply@senliteratura.com.br',
+        defaultReplyTo: 'carolinaribeirodacol@gmail.com',
+      },
+    },
+  },
 });
