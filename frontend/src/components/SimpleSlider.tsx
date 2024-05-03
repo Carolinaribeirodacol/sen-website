6e307fbf39cf44178ebbe119a4a2b7dce02b8650
Carolinaribeirodacol <carolinaribeirodacol@gmail.com>
2024-05-03 11:58:43 -0300

    feat: pacote de carrossel

--- frontend/src/components/SimpleSlider.tsx
+++ frontend/src/components/SimpleSlider.tsx
@@ -4,7 +4,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 export default function SimpleSlider({ children, className }: any) {
   const settings = {
     className,
